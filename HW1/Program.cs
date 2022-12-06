0c50648c4a4fde5b42d8b876262ec6a25b75450b
Drago <dragoko12@gmail.com>
2022-12-06 18:28:59 +0300

    add 1st task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"большее число: {num1} меньшее число:  {num2}");
+}
+else
+{
+    Console.WriteLine($"большее число: {num2} меньшее число:  {num1}");
+}
