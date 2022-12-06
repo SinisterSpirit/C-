436b5d622635531694e4b34f489f145c2bc0199b
Drago <dragoko12@gmail.com>
2022-12-06 19:02:54 +0300

    add 3rd task

---
+++ HW3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int test = num % 2;
+
+if (test != 0)
+{
+    Console.WriteLine($"{num} не чётное");
+}
+else
+{
+    Console.WriteLine($"{num} чётное");
+}
