b8c3b9b19f700953884d83364c90709f1173c59c
Drago <dragoko12@gmail.com>
2022-12-09 18:18:01 +0300

    add third task L2

---
+++ HW2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0 && num < 8)
+{
+    if (num < 6)
+    { 
+        Console.WriteLine("будний день ");
+    }
+    else
+    {
+        Console.WriteLine("выходной день ");
+    }
+}
+else
+{
+    Console.WriteLine("несуществующий день ");
+}
