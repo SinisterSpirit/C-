643911dbce1aa4f79d6c796f02583258c79a9427
Drago <dragoko12@gmail.com>
2022-12-09 17:34:33 +0300

    add first task L2

---
+++ HW2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 | num > 999)
+{
+    Console.WriteLine("Это не трёхзначное число, программа сломалась: ");
+}
+else
+{
+    int result = (num/10)%10;
+    Console.WriteLine($"вторая цифра числа {num} : {result} ");
+}
