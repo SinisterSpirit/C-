89d8cb8d11137890f1c7c6b24d5073f0ca8c7466
Drago <dragoko12@gmail.com>
2022-12-09 18:10:55 +0300

    add second task L2

---
+++ HW2_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("третьей цифры нет ");
+}
+else
+{
+    int len = 0;
+    int num2 = num;
+    while (num2 > 0)
+    {
+        num2 = num2 / 10;
+        len++;
+    }
+    int result = num / Convert.ToInt32(Math.Pow(10, len - 3)) % 10;
+    Console.WriteLine($"третья цифра числа {num} : {result} ");
+}
