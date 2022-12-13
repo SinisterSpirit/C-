7f3adde0673ead614e32e433391ea38328b144f8
Drago <dragoko12@gmail.com>
2022-12-13 19:19:21 +0300

    add third task L3

---
+++ HW3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{ 
+    int result = 0;
+    result = i*i*i;
+    Console.Write(result);
+    Console.Write(" ");
+}
